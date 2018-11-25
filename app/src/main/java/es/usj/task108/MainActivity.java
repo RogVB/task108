833b19e8e3be1fbd03472b34ceeee3cbcea0d9ba
Roger Vallejo <rogerbalbuena@hotmail.com>
2018-11-25 15:22:56 +0100

    Initial commit

---
+++ app/src/main/java/es/usj/task108/MainActivity.java
@@ -0,0 +1,22 @@
+package es.usj.task108;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvMensaje;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvMensaje = findViewById(R.id.tvMensaje);
+    }
+
+    public void llamar(View view) {
+        tvMensaje.setText("Llamando...");
+    }
+}
